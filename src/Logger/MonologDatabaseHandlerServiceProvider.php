8d01164af8a04ef4f5cc94bd34d2043cbed917c5
trent.schmidt <sch43228@gmail.com>
2020-12-05 10:01:58 -0600

    Test change

--- src/Logger/MonologDatabaseHandlerServiceProvider.php
+++ src/Logger/MonologDatabaseHandlerServiceProvider.php
@@ -51,13 +51,4 @@ class MonologDatabaseHandlerServiceProvider extends ServiceProvider
         });
     }
 
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return array();
-    }
 }
